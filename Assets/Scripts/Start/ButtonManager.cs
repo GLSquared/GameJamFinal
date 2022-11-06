b5d54bebffcef19089b91b341232cfd68e160566
lukemusu16 <19652002luke@gmail.com>
2022-11-06 13:31:21 +0100

    Button sounds added to start menu

--- Assets/Scripts/Start/ButtonManager.cs
+++ Assets/Scripts/Start/ButtonManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
+
+    AudioSource buttonPress;
+
+    private void Start()
+    {
+        buttonPress = GetComponent<AudioSource>();
+    }
+
     public void startGame()
     {
+        buttonPress.Play(0);
         SceneManager.LoadScene("BuildingScene");
     }
 
     public void quitGame()
     {
+        buttonPress.Play(0);
+
         Application.Quit();
     }
 }
